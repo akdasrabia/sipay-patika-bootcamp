4e49fe8d416c3802c58c764ec92902b11da4425a
akdasrabian <akdasrabian@gmail.com>
2023-07-25 12:54:48 +0300

    odev3 completed

---
+++ odev3/BookStore/Controllers/BookController.cs
@@ -0,0 +1,101 @@
+﻿using BookStore.BookOperations.CreateBook;
+using BookStore.BookOperations.DeleteBook;
+using BookStore.BookOperations.GetBooks;
+using BookStore.BookOperations.UpdateBook;
+using BookStore.DBOperations;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using static BookStore.BookOperations.CreateBook.CreateBookCommand;
+using static BookStore.BookOperations.UpdateBook.UpdateBookCommand;
+
+namespace BookStore.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BookController : ControllerBase
+    {
+        readonly BookStoreDbContext _context;
+        public BookController(BookStoreDbContext context)
+        {
+            _context = context;
+        }
+
+
+
+        [HttpGet]
+        public IActionResult GetBooks()
+        {
+            GetBooksQuery query = new GetBooksQuery(_context);
+            var result = query.Handle();
+            return Ok(result);
+        }
+
+        [HttpGet("{id}")]
+        public Book GetById(int id)
+        {
+            var book = _context.Books.Where(x => x.Id == id).SingleOrDefault();
+            return book;
+        }
+
+        //[HttpGet]
+        //public Book Get([FromQuery] string id)
+        //{
+        //    var book = BookList.Where(x => x.Id == Convert.ToInt32(id)).SingleOrDefault();
+        //    return book;
+        //}
+
+        [HttpPost]
+        public IActionResult AddBook([FromBody] CreateBookModel newBook)
+        {
+            
+            CreateBookCommand command = new CreateBookCommand(_context);
+            try
+            {
+                command.Model = newBook;
+                command.Handle();
+                return Ok();
+            }
+            catch(Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }  
+        }
+
+        [HttpPut]
+        public IActionResult UpdateBook(int id, [FromBody] UpdateBookModel updatedBook)
+        {
+
+            
+            UpdateBookCommand command = new UpdateBookCommand(_context);
+            try
+            {
+                command.BookId = id;
+                command.Model = updatedBook;
+                command.Handle();
+                return Ok();
+
+            }catch(Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+            
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult DeleteBook(int id) 
+        {
+            DeleteBookCommand command = new DeleteBookCommand(_context);
+            try
+            {
+                command.BookId = id;
+                command.Handle();
+                return Ok();
+            }
+            catch(Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+           
+        }
+    }
+}
